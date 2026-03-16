e32d7a9708b1bc6c3b1eaef9579c01de1e11b498
vimal2512 <ervimalraj96@gmail.com>
2026-03-16 21:14:34 +0530

    fix: add missing website repository functions

--- src/infrastructure/repositories/websiteRepository.js
+++ src/infrastructure/repositories/websiteRepository.js
@@ -1,20 +1,42 @@
 import Website from "../../domain/models/Website.js";
 
 /*
+CREATE WEBSITE
+*/
+export async function createWebsite(data) {
+
+  const trackingId =
+    "trk_" + Math.random().toString(36).substring(2, 12);
+
+  return Website.create({
+    ...data,
+    trackingId
+  });
+
+}
+
+/*
+GET ALL WEBSITES
+*/
+export async function getWebsites() {
+
+  return Website.find();
+
+}
+
+/*
 GET WEBSITE BY ID
 */
-
-export const getWebsiteById = (id) => {
+export async function getWebsiteById(id) {
 
   return Website.findById(id);
 
-};
+}
 
 /*
 UPDATE WEBSITE
 */
-
-export const updateWebsite = (id, data) => {
+export async function updateWebsite(id, data) {
 
   return Website.findByIdAndUpdate(
     id,
@@ -22,4 +44,13 @@ export const updateWebsite = (id, data) => {
     { new: true }
   );
 
-};
+}
+
+/*
+DELETE WEBSITE
+*/
+export async function deleteWebsite(id) {
+
+  return Website.findByIdAndDelete(id);
+
+}
